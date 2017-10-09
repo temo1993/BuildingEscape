64b7360bba02ac5534e1cdab9dc7d9c96451e7e0
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2017-10-09 17:07:01 +0400

    Getting position of object
    Getting position of StaticMeshActor objects

--- Source/BuildingEscape/PositionReport.cpp
+++ Source/BuildingEscape/PositionReport.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
+#include "Gameframework/Actor.h"
 
 
 // Sets default values for this component's properties
@@ -9,8 +10,6 @@ UPositionReport::UPositionReport()
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -19,8 +18,9 @@ void UPositionReport::BeginPlay()
 {
 	Super::BeginPlay();
 
-	// ...
-	
+	FString ObjectName = GetOwner()->GetName();
+	FString ObjectPos = GetOwner()->GetTransform().GetLocation().ToString();
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *ObjectName, *ObjectPos);
 }
 
 
