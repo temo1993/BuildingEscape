a2697228db922945a44b9913dbefe928122071cb
Temo Kiknavelidze <temo.kiknavelidze@gmail.com>
2017-10-17 22:48:01 +0400

    ...Input key support added..

--- Source/BuildingEscape/Grabber.h
+++ Source/BuildingEscape/Grabber.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "PhysicsEngine/PhysicsHandleComponent.h"
+#include "Components/InputComponent.h"
 #include "DrawDebugHelpers.h"
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
@@ -30,4 +31,12 @@ private:
 	float Reach = 100.f;
 	
 	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+
+	UInputComponent* InputComponent = nullptr;
+
+	// Ray-cast and grab what's in reach
+	void Grab();
+
+	// Called when grab is released
+	void Release();
 };
